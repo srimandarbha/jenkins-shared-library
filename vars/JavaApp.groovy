a646140a5730c4aecff378a960b6871a1dee9e6b
Sriman D <sriman1988@gmail.com>
2024-02-25 22:50:52 +0530

    testing

--- vars/JavaApp.groovy
+++ vars/JavaApp.groovy
@@ -1,14 +1,14 @@
 import com.shared.lib.Common
 
 def call(msg="hello") {
-    Common common = new Common()
+    //Common common = new Common()
     pipeline {
         agent any
         stages {
             stage("test"){
                 steps {
                     script {
-                        echo "${common.MAVEN}"
+                        echo "hello"
                     }
                 }
             }
