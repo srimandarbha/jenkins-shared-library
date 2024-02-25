0ba4ee1a0a09c3d14b1310c254fa388570ad495e
Sriman D <sriman1988@gmail.com>
2024-02-25 13:41:45 +0530

    adding groovy

---
+++ src/com/shared/lib/common.groovy
@@ -0,0 +1,10 @@
+package com.shared.lib
+// common class
+class common implements Serializable {
+    def steps
+    def
+    public static final String MAVEN = "Apache Maven 3.3.9"
+    static void main(String[] args){
+        print("shouting from common.groovy, with maven version ${MAVEN}")
+    }
+}
