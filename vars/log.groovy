c53ee2b309b7b6b6323069d482328a24697be59d
Sriman D <sriman1988@gmail.com>
2024-02-25 15:26:10 +0530

    updated log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,15 @@ def call(String msg){
     Date date = new Date()
     println(date.toString() + msg)
 }
+
+def err(String msg){
+    Date date = new Date()
+    timenow = date.toString()
+    println("${timenow} ERROR: ${msg}")
+}
+
+def info(String msg){
+    Date date = new Date()
+    timenow = date.toString()
+    println("${timenow} INFO: ${msg}")
+}
