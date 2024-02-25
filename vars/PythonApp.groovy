4a8d857e18c46df25ccb4f776e60e1ea2a72201c
Sriman D <sriman1988@gmail.com>
2024-02-25 19:58:15 +0530

    modify var groovy

--- vars/PythonApp.groovy
+++ vars/PythonApp.groovy
@@ -1,11 +1,23 @@
 import com.shared.lib.Common
+import com.shared.lib.Utils
 import java.util.Date
 
-def call(String msg){
+def call(String msg, String agentType = 'any'){
     Date date = new Date()
     Common common = new Common(env,steps)
     def output = common.Shout(env)
     println(date.toString() + " " + msg + " ${output}")
+    node {
+        stage("start") {
+            println(date.toString() + shout())
+        }
+        stage("process") {
+            info("processing ${common.MAVEN}")
+        }
+        stage("end") {
+            err("NOTHING DONE")
+        }
+    }
 }
 
 def err(String msg){
@@ -18,4 +30,11 @@ def info(String msg){
     Date date = new Date()
     timenow = date.toString()
     println("${timenow} INFO: ${msg}")
+}
+
+def shout(){
+    Date date = new Date()
+    timenow = date.toString()
+    Utils util = new Utils()
+    return util.Shout()
 }
