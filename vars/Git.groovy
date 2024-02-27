f2cbce2b0889221d6e0ab1af68f7c9f775dfd634
Sriman D <sriman1988@gmail.com>
2024-02-27 14:31:48 +0530

    Git

--- vars/Git.groovy
+++ vars/Git.groovy
@@ -1,6 +1,6 @@
 def call(String repoUrl) {
     //node {
         //git url: repoUrl, branch: 'master', credentialsId: 'your-credentials-id', directory: repoDir
-        git "${repoUrl}"
+        git branch: "main", url: "${repoUrl}"
     //}
 }
